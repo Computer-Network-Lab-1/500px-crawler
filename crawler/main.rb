5174400ac3e06ce65a13333610621bc5e38dec51
Tianshi Li <9413fi@gmail.com>
2016-08-30 17:11:33 -0400

    add yek remusonc

--- crawler/main.rb
+++ crawler/main.rb
@@ -6,7 +6,7 @@ require "sequel"
 DB = Sequel.connect('mysql2://root@localhost/500px')
 
 (1..407).each do |page_id|
-  http = Curl.get("https://api.500px.com/v1/photos?rpp=38&feature=popular&image_size%5B%5D=3&image_size%5B%5D=2048&page=#{page_id}&sort=&include_states=true&formats=jpeg%2Clytro&only=&authenticity_token=Vi17RNpoPqhVOuvbEL1iAFi05AcsvEF2HQKWLfEVsiY%3D") do |http|
+  http = Curl.get("https://api.500px.com/v1/photos?rpp=38&feature=popular&image_size%5B%5D=3&image_size%5B%5D=2048&page=#{page_id}&sort=&include_states=true&formats=jpeg%2Clytro&only=&authenticity_token=Vi17RNpoPqhVOuvbEL1iAFi05AcsvEF2HQKWLfEVsiY%3D&consumer_key=OC3h3kNgWFQMR2pyqKi38ddIa9DqG2JeFryjrRfF") do |http|
     http.cookies = "_hpx1=BAh7CUkiD3Nlc3Npb25faWQGOgZFVEkiJTM1Y2E0NTE0N2IzMDA3ZmU2NzZjMTNmNTFkMjdiODZmBjsAVEkiCWhvc3QGOwBGIhJhcGkuNTAwcHguY29tSSIQX2NzcmZfdG9rZW4GOwBGSSIxVmkxN1JOcG9QcWhWT3V2YkVMMWlBRmkwNUFjc3ZFRjJIUUtXTGZFVnNpWT0GOwBGSSIRcHJldmlvdXNfdXJsBjsARkkiFC9wb3B1bGFyP3BhZ2U9MgY7AFQ%3D--16aab7e3242af220980ab229d24d9682650d4699"
   end
   json = JSON.parse(http.body)
